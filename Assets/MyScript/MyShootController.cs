66c85f12fddbf6b57840cb418fd8c7c88683dab1
bello8131 <77046927+bello8131@users.noreply.github.com>
2021-06-05 15:14:17 +0800

    添加魔力消耗和魔力條(寫在MyShootController.cs裡)

--- Assets/MyScript/MyShootController.cs
+++ Assets/MyScript/MyShootController.cs
@@ -8,10 +8,17 @@ public class MyShootController : MonoBehaviour
     public TimeAreaManager timeAreaManager_speedUp;
     public TimeAreaManager timeAreaManager_slowDown;
     public TimeAreaManager timeAreaManager_backward;
-
     public TimeAreaManager timeBoxManager_pause;
     public TimeAreaManager timeBoxManager_speedUp;
     public TimeAreaManager timeBoxManager_slowDown;
+
+    public float timeAreaManager_pause_consuming;
+    public float timeAreaManager_speedUp_consuming;
+    public float timeAreaManager_slowDown_consuming;
+    public float timeAreaManager_backward_consuming;
+    public float timeBoxManager_pause_consuming;
+    public float timeBoxManager_speedUp_consuming;
+    public float timeBoxManager_slowDown_consuming;
 
     public TimeAreaManager timeBoxManager_bong;
 
@@ -19,12 +26,18 @@ public class MyShootController : MonoBehaviour
 
     public MyTimeState shootType = MyTimeState.Pause;
 
+    public float maxMagic;
+    public float currentMagic;
+    public float magicRecoverySpeed;
+
     private void Start()
     {
         if (lockMouse)
         {
             Cursor.lockState = CursorLockMode.Locked;
         }
+
+        currentMagic = maxMagic;
     }
 
     private void Update()
@@ -64,6 +77,14 @@ public class MyShootController : MonoBehaviour
         {
             shootType = MyTimeState.Normal;
         }
+
+        if (currentMagic < maxMagic)
+        {
+            currentMagic += magicRecoverySpeed * Time.deltaTime;
+        }else
+        {
+            currentMagic = maxMagic;
+        }
     }
 
     private void ShootTheSkill()
@@ -76,16 +97,31 @@ public class MyShootController : MonoBehaviour
             switch (shootType)
             {
                 case MyTimeState.Pause:
-                    timeAreaManager_pause.CreateTimeArea(transform.position, Quaternion.identity, hit.point);
+                    if (currentMagic >= timeAreaManager_pause_consuming) {
+                        currentMagic -= timeAreaManager_pause_consuming;
+                        timeAreaManager_pause.CreateTimeArea(transform.position, Quaternion.identity, hit.point);
+                    }
                     break;
                 case MyTimeState.SpeedUp:
-                    timeAreaManager_speedUp.CreateTimeArea(transform.position, Quaternion.identity, hit.point);
+                    if (currentMagic >= timeAreaManager_speedUp_consuming)
+                    {
+                        currentMagic -= timeAreaManager_speedUp_consuming;
+                        timeAreaManager_speedUp.CreateTimeArea(transform.position, Quaternion.identity, hit.point);
+                    }
                     break;
                 case MyTimeState.SlowDown:
-                    timeAreaManager_slowDown.CreateTimeArea(transform.position, Quaternion.identity, hit.point);
+                    if (currentMagic >= timeAreaManager_slowDown_consuming)
+                    {
+                        currentMagic -= timeAreaManager_slowDown_consuming;
+                        timeAreaManager_slowDown.CreateTimeArea(transform.position, Quaternion.identity, hit.point);
+                    }
                     break;
                 case MyTimeState.BackWard:
-                    timeAreaManager_backward.CreateTimeArea(transform.position, Quaternion.identity, hit.point);
+                    if (currentMagic >= timeAreaManager_backward_consuming)
+                    {
+                        currentMagic -= timeAreaManager_backward_consuming;
+                        timeAreaManager_backward.CreateTimeArea(transform.position, Quaternion.identity, hit.point);
+                    }
                     break;
                 case MyTimeState.Normal:
                     break;
@@ -104,15 +140,26 @@ public class MyShootController : MonoBehaviour
             switch (shootType)
             {
                 case MyTimeState.Pause:
-                    timeBoxManager_pause.CreateTimeArea(hit.point, Quaternion.identity, hit.point);
+                    if (currentMagic >= timeBoxManager_pause_consuming)
+                    {
+                        currentMagic -= timeBoxManager_pause_consuming;
+                        timeBoxManager_pause.CreateTimeArea(hit.point, Quaternion.identity, hit.point);
+                    }
                     break;
                 case MyTimeState.SpeedUp:
-                    timeBoxManager_speedUp.CreateTimeArea(hit.point, Quaternion.identity, hit.point);
+                    if (currentMagic >= timeBoxManager_speedUp_consuming)
+                    {
+                        currentMagic -= timeBoxManager_speedUp_consuming;
+                        timeBoxManager_speedUp.CreateTimeArea(hit.point, Quaternion.identity, hit.point);
+                    }
                     break;
                 case MyTimeState.SlowDown:
-                    timeBoxManager_slowDown.CreateTimeArea(hit.point, Quaternion.identity, hit.point);
+                    if (currentMagic >= timeBoxManager_slowDown_consuming)
+                    {
+                        currentMagic -= timeBoxManager_slowDown_consuming;
+                        timeBoxManager_slowDown.CreateTimeArea(hit.point, Quaternion.identity, hit.point);
+                    }
                     break;
-              
             }
             //timeAreaManager.CreateTimeArea(transform.position, Quaternion.identity, hit.point);
         }
