d2d5ab253feb06cfb7912f8613a8ccd2146ad7a6
bello8131 <77046927+bello8131@users.noreply.github.com>
2021-06-08 01:15:53 +0800

    修改開始畫面的UI

---
+++ Assets/MyScript/MyExplainGameplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyExplainGameplay : MonoBehaviour
+{
+    private bool isExplain = false;
+    public GameObject startMenu;
+    public GameObject explainMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isExplain && Input.GetKeyDown(KeyCode.Escape))
+        {
+            isExplain = false;
+            startMenu.SetActive(true);
+            explainMenu.SetActive(false);
+        }
+    }
+
+    public void ShowExplain()
+    {
+        isExplain = true;
+        startMenu.SetActive(false);
+        explainMenu.SetActive(true);
+    }
+}
