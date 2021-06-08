2a935ea406b7fb430cde4711f0b7930711b26828
a15121212 <a14736920@gmail.com>
2021-06-09 02:03:28 +0800

    PlayScene音效

--- Assets/Scripts/SoundPlay/PlaySceneSound.cs
+++ Assets/Scripts/SoundPlay/PlaySceneSound.cs
@@ -3,16 +3,139 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlaySceneSound : MonoBehaviour
-{
-    // Start is called before the first frame update
+{   
+    // Magic Related
+    public AudioClip LackMP;
+    public AudioClip ChangeMagic;
+    public AudioClip ShootMagicSpeedup;
+    public AudioClip ShootMagicSlowDown;
+    public AudioClip ShootMagicStop;
+    public AudioClip ShootMagicRewind;
+    public AudioClip ShootMagicBomb;
+
+    //Shot Related
+    public AudioClip Speedup_shot;
+    public AudioClip SlowDown_shot;
+    public AudioClip Stop_shot;
+    public AudioClip Rewind_shot;
+
+    // Move Related
+    public AudioClip Run;
+    public AudioClip WoodCollision;
+    public AudioClip StoneCollision;
+
+    // Thorw Relate
+    public AudioClip Throw;
+    public AudioClip IntoWater;
+
+
+    AudioSource audioSource;
+
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+
+    }
+    
+    // Magic Related
+    public void LackMpSound()
+    {
+        audioSource.PlayOneShot(LackMP);
+
+    }
+    public void ChangeMagicSound()
+    {
+        audioSource.PlayOneShot(ChangeMagic);
+
+
+    }
+    public void ShootMagic(int magic_number)
+    {
+        switch (magic_number)
+        {
+            case 1:
+                audioSource.PlayOneShot(ShootMagicStop);
+                break;
+            case 2:
+                audioSource.PlayOneShot(ShootMagicSpeedup);
+                break;
+            case 3:
+                audioSource.PlayOneShot(ShootMagicSlowDown);
+                break;
+            case 4:
+                audioSource.PlayOneShot(ShootMagicRewind);
+                break;
+            case 5:
+                audioSource.PlayOneShot(ShootMagicBomb);
+                break;
+            default:
+                break;
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Get shot
+    public void GetShot(int magic_number)
     {
-        
+        switch (magic_number)
+        {
+            case 1:
+                audioSource.PlayOneShot(Stop_shot);
+                break;
+            case 2:
+                audioSource.PlayOneShot(Speedup_shot);
+                break;
+            case 3:
+                audioSource.PlayOneShot(SlowDown_shot);
+                break;
+            case 4:
+                audioSource.PlayOneShot(Rewind_shot);
+                break;
+            /*
+            case 5:
+                audioSource.PlayOneShot(ShootMagicBomb);
+                break;
+            */
+            default:
+                break;
+
+        }
+
+    }
+
+    // Move related
+    public void RunSound()
+    {
+        audioSource.PlayOneShot(Run);
+    }
+    public void WoodCollisionSound()
+    {
+        audioSource.PlayOneShot(WoodCollision);
+    }
+    public void StoneCollisionSound()
+    {
+        audioSource.PlayOneShot(StoneCollision);
+    }
+
+    // Throw Related
+    public void ThrowSound()
+    {
+        audioSource.PlayOneShot(Throw);
+    }
+    public void IntoWaterSound()
+    {
+        audioSource.PlayOneShot(IntoWater);
+    }
+    public void FlySound()
+    {
+        audioSource.PlayOneShot(Fly);
+    }
+
+
+
+    public void Stop()
+    {
+        audioSource.Stop();
+
     }
 }
