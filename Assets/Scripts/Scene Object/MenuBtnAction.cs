d8d4c51a3679e55c863672d3f678caf63a98c155
a15121212 <a14736920@gmail.com>
2021-06-08 18:06:23 +0800

    卓然的package
    在0607

--- Assets/Scripts/Scene Object/MenuBtnAction.cs
+++ Assets/Scripts/Scene Object/MenuBtnAction.cs
@@ -43,7 +43,7 @@ public class MenuBtnAction : MonoBehaviour
     void Start()
     {
     
-    SoundPlayer = (GameObject.Find("SoundPlayer")).GetComponent<StartUIBGM>();
+         SoundPlayer = (GameObject.Find("SoundPlayer")).GetComponent<StartUIBGM>();
        
     }
 
