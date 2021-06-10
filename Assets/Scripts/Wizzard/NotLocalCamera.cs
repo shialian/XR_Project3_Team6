66a1671a734f50ce1e52e24ea19a5adbbfed1ee7
shialian <50765318+shialian@users.noreply.github.com>
2021-06-10 13:50:53 +0800

    Camera Problem Test

--- Assets/Scripts/Wizzard/NotLocalCamera.cs
+++ Assets/Scripts/Wizzard/NotLocalCamera.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public class PCCamera : NetworkBehaviour
+public class NotLocalCamera : NetworkBehaviour
 {
     public GameObject cam;
+    public GameObject OVRCam;
 
     private void Update()
     {
         if(!isLocalPlayer)
         {
             cam.SetActive(false);
+            OVRCam.SetActive(false);
             this.enabled = false;
         }
     }
